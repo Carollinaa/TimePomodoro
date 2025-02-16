78d50d59a491e739f98ee8c2830ddde5f1c2eaf6
Carollinaa <carolina.ant.16@gmail.com>
2025-02-16 12:43:27 -0300

    add logica ao displey

--- pomodoro.c
+++ pomodoro.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include "pico/stdlib.h"
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "hardware/pwm.h"
+#include "hardware/i2c.h"
 
 // Biblioteca gerada pelo arquivo .pio durante compilação.
 #include "ws2818b.pio.h"
+#include "inc/ssd1306.h"
 
 // Definição do número de LEDs e pinos.
 #define LED_COUNT 25
@@ -13,19 +18,24 @@
 #define BTN_A 05
 #define BTN_B 06
 #define BUZZER_PIN 21
+#define SCREEN_WIDTH 128 // Largura do display SSD1306
+#define CHAR_WIDTH 6     // Largura média de um caractere na fonte padrão
+
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
 
 volatile bool inicializado = false;
 int ciclos = 4;
 int ciclo_atual = 0;
+int tempo_restante = 0;
 
 struct repeating_timer timer;
 struct repeating_timer timer2;
-struct repeating_timer timer3;
 
 // Protótipos das funções (declaração antes da implementação)
-bool iniciar(struct repeating_timer *t);
-bool pausa_longa(struct repeating_timer *t);
-bool pausa_curta(struct repeating_timer *t);
+void display_text(uint8_t *ssd, char *text[]);
+void desenhar_sprite(int matriz[5][5][3]);
+uint8_t ssd[ssd1306_buffer_length];
 
 // Definição de pixel GRB
 struct pixel_t
@@ -42,7 +52,7 @@ npLED_t leds[LED_COUNT];
 PIO np_pio;
 uint sm;
 
-//Inicializa a máquina PIO para controle da matriz de LEDs.
+// Inicializa a máquina PIO para controle da matriz de LEDs.
 void npInit(uint pin)
 {
   // Cria programa PIO.
@@ -67,7 +77,7 @@ void npInit(uint pin)
     leds[i].B = 0;
   }
 }
-//Atribui uma cor RGB a um LED.
+// Atribui uma cor RGB a um LED.
 void npSetLED(const uint index, const uint8_t r, const uint8_t g, const uint8_t b)
 {
   leds[index].R = r;
@@ -82,7 +92,7 @@ void npClear()
     npSetLED(i, 0, 0, 0);
 }
 
-//Escreve os dados do buffer nos LEDs.
+// Escreve os dados do buffer nos LEDs.
 void npWrite()
 {
   // Escreve cada dado de 8-bits dos pixels em sequência no buffer da máquina PIO.
@@ -111,7 +121,7 @@ int getIndex(int x, int y)
   }
 }
 
-//matriz de led do tempo de foco
+// matriz de led do tempo de foco
 int matriz_foco[5][5][3] =
     {
         {{0, 0, 0}, {0, 100, 0}, {0, 100, 0}, {0, 100, 0}, {0, 0, 0}},
@@ -142,7 +152,8 @@ int matriz_pausa_longa[5][5][3] =
 
 };
 
-void beep(int frequency){
+void beep(int frequency)
+{
   uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN); // Obtém o slice do PWM
   uint wrap_value = 125000000 / frequency;            // Define o valor de wrap com base na frequência
 
@@ -154,13 +165,78 @@ void beep(int frequency){
 bool stop_beep(struct repeating_timer *t)
 {
   uint slice_num = pwm_gpio_to_slice_num(BUZZER_PIN); // Obtém o slice do PWM
-  pwm_set_enabled(slice_num, false); // Desliga o PWM
+  pwm_set_enabled(slice_num, false);                  // Desliga o PWM
   return false;
 }
-// converte milissegundos em segundods
-int tempoEmSegundos(int segundos)
+
+bool foco = true;
+char text_fase[20];
+
+bool start(struct repeating_timer *t)
 {
-  return segundos * 1000;
+  if (!inicializado)
+  {
+    tempo_restante = 0;
+    foco = true;
+    return false;
+  }
+  if (tempo_restante <= 0)
+  {
+    ciclo_atual++;
+    if (foco)
+    {
+      strcpy(text_fase, "Tempo de Foco");
+      desenhar_sprite(matriz_foco);
+      tempo_restante = tempo_em_minutos(25);
+      foco = false;
+    }
+    else
+    {
+      foco = true;
+      if (ciclo_atual < ciclos)
+      {
+        strcpy(text_fase, "Pausa Curta");
+        desenhar_sprite(matriz_pausa_curta);
+        tempo_restante = tempo_em_minutos(5);
+      }
+      else
+      {
+        strcpy(text_fase, "Pausa Longa");
+        desenhar_sprite(matriz_pausa_longa);
+        tempo_restante = tempo_em_minutos(15);
+        ciclo_atual = 0;
+      }
+    }
+
+  }
+
+  // Converte segundos para MM:SS
+  int minutos = tempo_restante / 60;
+  int segundos = tempo_restante % 60;
+
+  char tempo[10];
+  sprintf(tempo, "%02d:%02d", minutos, segundos); // Formata o tempo
+
+  // Texto para exibição
+  char *text[] = {
+      "   Bem-vindos",
+      " ",
+      "  Ao Pomodoro",
+      " ",
+      " ",
+      text_fase,
+      " ",
+      tempo};
+  display_text(ssd, text);
+
+  tempo_restante--; // Decrementa 1 segundo
+  return true;      // Mantém o timer rodando
+}
+
+// converte milissegundos em segundods
+int tempo_em_minutos(int minutos)
+{
+  return minutos * 60;
 }
 // inicializa os inputs
 void init_input(int input)
@@ -176,11 +252,11 @@ void init_output(int output)
   gpio_set_dir(output, GPIO_OUT);
 }
 
- // Desenha os Sprites
+// Desenha os Sprites
 void desenhar_sprite(int matriz[5][5][3])
 {
-   beep(2000);
-  add_repeating_timer_ms(200, stop_beep, NULL, &timer3);
+  beep(2000);
+  add_repeating_timer_ms(1000, stop_beep, NULL, &timer2);
   for (int linha = 0; linha < 5; linha++)
   {
     for (int coluna = 0; coluna < 5; coluna++)
@@ -193,49 +269,6 @@ void desenhar_sprite(int matriz[5][5][3])
   npWrite(); // Escreve os dados nos LEDs.
 }
 
-bool pausa_curta(struct repeating_timer *t)
-{
-  if (inicializado)
-  {
-
-    ciclo_atual++;
-    if (ciclo_atual < ciclos)
-    {
-      desenhar_sprite(matriz_pausa_curta);
-      add_repeating_timer_ms(1000, iniciar, NULL, &timer2);
-    }
-    else
-    {
-      desenhar_sprite(matriz_pausa_longa);
-      add_repeating_timer_ms(5000, iniciar, NULL, &timer2);
-      ciclo_atual = 0;
-    }
-  }
-
-  return false; // Retorna falso para executar apenas uma vez
-}
-
-bool pausa_longa(struct repeating_timer *t)
-{
-  if (inicializado)
-  {
-    add_repeating_timer_ms(1000, iniciar, NULL, &timer);
-  }
-
-  return false;
-}
-
-bool iniciar(struct repeating_timer *t)
-{
-  if (inicializado)
-  {
-    desenhar_sprite(matriz_foco);
-    add_repeating_timer_ms(1000, pausa_curta, NULL, &timer);
-  }
-
-  return false;
-}
-
 void desligar()
 {
   npClear();
@@ -243,11 +276,62 @@ void desligar()
   inicializado = false;
 }
 
+zerar_display(struct render_area frame_area)
+{
+
+  calculate_render_area_buffer_length(&frame_area);
+  // Zera o display
+  memset(ssd, 0, ssd1306_buffer_length);
+  render_on_display(ssd, &frame_area);
+}
+
+void display_text(uint8_t *ssd, char *text[])
+{
+
+  struct render_area frame_area = {
+    start_column : 0,
+    end_column : ssd1306_width - 1,
+    start_page : 0,
+    end_page : ssd1306_n_pages - 1
+  };
+
+  calculate_render_area_buffer_length(&frame_area);
+  // Zera o display
+  memset(ssd, 0, ssd1306_buffer_length);
+  render_on_display(ssd, &frame_area);
+
+  int y = 0;
+  for (uint i = 0; i < (count_of(text) * 8); i++)
+  {
+    // Verifica se há espaço na tela para mais uma linha de texto
+    if (y >= ssd1306_height)
+    {
+      break; // Se exceder a altura do display, interrompe
+    }
+
+    // Desenha o texto na posição correta
+    ssd1306_draw_string(ssd, 5, y, text[i]);
+    y += 8; // Move para a próxima linha (8 pixels de altura)
+  }
+
+  render_on_display(ssd, &frame_area); // Atualiza a tela
+}
+
 int main()
 {
 
   init_input(BTN_A);
   init_input(BTN_B);
+
+  // Inicialização do i2c
+  // i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+  gpio_pull_up(I2C_SDA);
+  gpio_pull_up(I2C_SCL);
+
+  // Processo de inicialização completo do OLED SSD1306
+  // ssd1306_init();
 
   // Inicializa o GPIO como saída PWM
   gpio_set_function(BUZZER_PIN, GPIO_FUNC_PWM);
@@ -259,7 +343,7 @@ int main()
   npInit(LED_PIN);
   npClear();
 
-  npWrite();// Escreve os dados nos LEDs.
+  npWrite(); // Escreve os dados nos LEDs.
 
   while (true)
   {
@@ -267,7 +351,8 @@ int main()
     if (!gpio_get(BTN_A) && !inicializado)
     {
       inicializado = true;
-      add_repeating_timer_ms(50, iniciar, NULL, &timer);
+      // add_repeating_timer_ms(50, iniciar, NULL, &timer);
+      add_repeating_timer_ms(-1000, start, NULL, &timer);
       sleep_ms(50);
     }
     if (!gpio_get(BTN_B) && inicializado)
